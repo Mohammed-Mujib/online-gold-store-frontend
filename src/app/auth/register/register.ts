3b63f7a22522e495ef5c47d48917d0d9b42d7676
Mohammed-Mujib <wadmujib@gmail.com>
2025-11-23 23:53:44 +0200

    init register design

--- src/app/auth/register/register.ts
+++ src/app/auth/register/register.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
 import { FieldsetModule } from 'primeng/fieldset';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-register',
-  imports: [FieldsetModule],
+  imports: [FieldsetModule, TranslatePipe, TranslateModule, InputTextModule ],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
-export class Register {
-
-}
+export class Register {}
